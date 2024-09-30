ea7479950d013a7a71cdb59ea0a2325632b3a9ec
Jamcda <jamcdaniels99@gmail.com>
2024-09-29 21:31:16 -0400

    Nav bar links work

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
+#Jacob McDaniels, 9/29/2024, 8:12pm
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('blog/home', views.home, name='home'),
+    path('blog/post_list', views.post_list, name='post_list'),
+    path('blog/register', views.register, name='register'),
+    path('blog/faq', views.faq, name='faq'),
 ]
+#Jacob McDaniels, 9/29/2024, 9:30pm
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,24 @@
+#Jacob McDaniels, 9/29/2024, 8:12pm
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+def home(request):
+    context = locals()
+    template = 'blog/home.html'
+    return render(request, template, context)
+
+def register(request):
+    context = locals()
+    template = 'blog/register.html'
+    return render(request, template, context)
+
+def faq(request):
+    context = locals()
+    template = 'blog/faq.html'
+    return render(request, template, context)
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+#Jacob McDaniels, 9/29/2024, 8:12pm
