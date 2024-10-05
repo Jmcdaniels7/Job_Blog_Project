d070099e9a26e68ced9fccee75ddb084fdc3b371
Jamcda <jamcdaniels99@gmail.com>
2024-10-05 11:08:31 -0400

    new

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('blog/home', views.homepage, name='home'),
     path('blog/post_list', views.post_list, name='post_list'),
-    path('blog/register', views.register, name='register'),
+    path('blog/register/', views.register, name='register'),
     path('blog/faq', views.faq, name='faq'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 #Jacob McDaniels, 9/29/2024, 9:30pm
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 #Jacob McDaniels, 9/29/2024, 8:12pm
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from django.contrib.auth.models import User, auth
+from .forms import PostForm
 
 def home(request):
     context = locals()
@@ -14,9 +16,23 @@ def homepage(request):
     return render(request, template, context)
 
 def register(request):
-    context = locals()
-    template = 'blog/register.html'
-    return render(request, template, context)
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmpassword = request.POST['confirmpassword']
+
+        if password==confirmpassword:
+            if User.object.filter(email=email).exists():
+                print('Email already in use')
+            else:
+                user = User.objects.create_user(firstname=firstname, lastname=lastname, email=email, password=password, confirmpassword=confirmpassword)
+                user.save()
+        else:
+            print('Passwords do not match!')
+
+    return render(request, 'blog/register.html')
 
 def faq(request):
     context = locals()
@@ -26,4 +42,35 @@ def faq(request):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-#Jacob McDaniels, 9/30/2024, 3:39pm
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+#Jacob McDaniels, 10/5/2024, 10:44am
